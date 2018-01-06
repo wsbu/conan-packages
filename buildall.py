67db97e335f02838540f89b05d50cbc710b037e5
David Zemon <david@zemon.name>
2018-01-05 21:53:02 -0600

    Add script for building all packages

---
+++ buildall.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# @file    buildall.py
+# @author  David Zemon
+#
+# Created with: PyCharm
+
+"""
+@description:
+"""
+
+import os
+import subprocess
+import typing
+
+CHANNEL = 'wsbu/stable'
+
+
+def run() -> None:
+    root = os.path.dirname(__file__)
+    directories = [os.path.join(root, d) for d in os.listdir(root) if os.path.isdir(d)]
+    conan_dirs = [d for d in directories if os.path.exists(os.path.join(d, 'conanfile.py'))]
+
+    for d in conan_dirs:
+        execute(['conan', 'create', CHANNEL], cwd=d)
+
+        completed_process = subprocess.run(['conan', 'info', d, '--only', 'None'], stdout=subprocess.PIPE)
+        package = completed_process.stdout.decode().split()[0].split('@')[0]
+        execute(['conan', 'upload', '--force', '--confirm', '--remote', 'ci', package + '@' + CHANNEL], cwd=d)
+
+
+def execute(args: typing.List, cwd: str, echo: bool=True) -> None:
+    if echo:
+        print('cd ' + cwd + ' && ' + ' '.join(args))
+    # subprocess.check_call(args, cwd=cwd)
+
+
+run()
