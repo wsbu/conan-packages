d16a1174c6cd79d91f6ad7b5f46e64ec511754a1
David Zemon <david.zemon@redlion.net>
2018-01-09 15:03:09 -0600

    Don't look for libnl... it just causes problems on arch

--- libpcap/conanfile.py
+++ libpcap/conanfile.py
@@ -9,8 +9,11 @@ class libpcapConan(ConanFile):
     description = 'the LIBpcap interface to various kernel packet capture mechanism'
     settings = 'os', 'compiler', 'build_type', 'arch'
     license = 'BSD'
-    options = {'shared': [True, False]}
-    default_options = 'shared=True'
+    options = {
+        'shared': [True, False],
+        'with_libnl': [True, False]
+    }
+    default_options = 'shared=True', 'with_libnl=False'
     generators = 'cmake'
 
     def source(self):
@@ -21,7 +24,8 @@ class libpcapConan(ConanFile):
 
         args = [
             '--prefix', self.package_folder,
-            '--with-pcap=' + self.settings.get_safe('os').lower()
+            '--with-pcap=' + self.settings.get_safe('os').lower(),
+            '--with%s-libnl' % ('' if self.options.with_libnl else 'out')
         ]
         os.environ['PATH'] += os.pathsep + os.pathsep.join(self.deps_cpp_info.bindirs)
         env.configure(configure_dir=(os.path.join(self.source_folder, 'libpcap')), args=args)
