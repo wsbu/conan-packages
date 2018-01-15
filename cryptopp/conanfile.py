d4048f696cb599933a0fd5188d65e665f91f9f5e
David Zemon <david@zemon.name>
2018-01-15 11:26:16 -0600

    Don't use absolute paths in cyrptopp package

--- cryptopp/conanfile.py
+++ cryptopp/conanfile.py
@@ -11,29 +11,34 @@ class CryptoPPConan(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
     license = 'Boost Software License 1.0'
     generators = 'cmake'
-    options = {'shared': [True, False]}
-    default_options = 'shared=True'
+    options = {
+        'shared': [True, False],
+        'cxx11': [True, False],
+    }
+    default_options = 'shared=True', 'cxx11=True'
+
+    ARCHIVE_BASENAME = name.upper() + '_' + version.replace('.', '_')
+    FOLDER = name + '-' + ARCHIVE_BASENAME
 
     def source(self):
-        zipname = 'CRYPTOPP_5_6_5.zip'
+        zipname = self.ARCHIVE_BASENAME + '.zip'
         url = 'https://github.com/weidai11/cryptopp/archive/%s' % zipname
-        sha256 = 'c934d2c427a0ef197ea989a00f7b6d866d110dd55257d2944d0513b382b7e2b4'
         tools.download(url, zipname)
-        tools.check_sha256(zipname, sha256)
         tools.unzip(zipname)
         os.remove(zipname)
 
         # Without this, CMake won't know how to find the dependencies that Conan is trying to inject
-        tools.replace_in_file('cryptopp-CRYPTOPP_5_6_5/CMakeLists.txt', 'project(cryptopp)',
+        tools.replace_in_file(os.path.join(self.FOLDER, 'CMakeLists.txt'), 'project(cryptopp)',
                               '''project(cryptopp)
 include("${PROJECT_BINARY_DIR}/conanbuildinfo.cmake")
 conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions['CMAKE_INSTALL_PREFIX'] = '/'
         cmake.configure(source_dir='cryptopp-CRYPTOPP_5_6_5')
         cmake.build()
-        cmake.install()
+        cmake.install(args=['--', 'DESTDIR=' + self.package_folder])
 
     def package_info(self):
         self.cpp_info.libs = ['cryptopp']
