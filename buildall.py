9ed6b59867d57afa4ad3f5b0ecffdf05fadbfd6b
David Zemon <david@zemon.name>
2018-02-05 13:11:49 -0600

    Attempt to fix libxml2

--- buildall.py
+++ buildall.py
@@ -13,6 +13,16 @@ import os
 import subprocess
 import sys
 
+try:
+    from shutil import which
+except ImportError:
+    def which(file_name):
+        for path in os.environ["PATH"].split(os.pathsep):
+            full_path = os.path.join(path, file_name)
+            if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+                return full_path
+        return None
+
 CHANNEL = 'wsbu/stable'
 
 
@@ -23,8 +33,14 @@ def run():
     directories = [os.path.join(root, d) for d in os.listdir(root) if os.path.isdir(d)]
     conan_dirs = [os.path.abspath(d) for d in directories if os.path.exists(os.path.join(d, 'conanfile.py'))]
 
+    conan_exe_args = ['python', '-u', which('conan')]
+
     for d in conan_dirs:
-        p = subprocess.Popen(['conan', 'info', d, '--only', 'None'], stdout=subprocess.PIPE)
+        print('#' * 80)
+        print('### Building {0}')
+        print('#' * 80)
+
+        p = subprocess.Popen(conan_exe_args + ['info', d, '--only', 'None'], stdout=subprocess.PIPE)
         return_code = p.wait()
         if 0 != return_code:
             if p.stdout:
@@ -37,11 +53,10 @@ def run():
             this_project = [line for line in lines if line.endswith('@PROJECT')][0]
             package = this_project.split('@')[0]
 
-            execute(
-                ['conan', 'create', d, CHANNEL, '--build', 'missing', '--build', 'outdated', '--update'] +
-                get_options(d)
-            )
-            execute(['conan', 'upload', '--force', '--confirm', '--remote', 'ci', '--all', package + '@' + CHANNEL])
+            execute(conan_exe_args + ['create', d, CHANNEL, '--build', 'missing', '--build', 'outdated', '--update']
+                    + get_options(d))
+            execute(conan_exe_args + ['upload', '--force', '--confirm', '--remote', 'ci', '--all', package + '@' +
+                                      CHANNEL])
 
 
 def get_options(d):
