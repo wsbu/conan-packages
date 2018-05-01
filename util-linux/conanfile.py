d8fbaf402d62cd0a73cc017bf976de2ff729de16
David Zemon <david@zemon.name>
2018-05-01 16:35:51 -0500

    Update util-linux to depend on linux-pam and bump version

--- util-linux/conanfile.py
+++ util-linux/conanfile.py
@@ -1,36 +1,49 @@
 import os
 
-from conans import ConanFile, AutoToolsBuildEnvironment, tools, RunEnvironment
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 
 
 class UtilLinuxConan(ConanFile):
     name = 'util-linux'
-    version = '1.18.0'
+    version = '2.30.2'
     url = 'https://github.com/wsbu/conan-packages'
     description = "Linux utilities"
     settings = 'os', 'compiler', 'build_type', 'arch'
     license = 'GNU GPLv2'
 
+    requires = 'linux-pam/1.2.1@wsbu/stable'
+
     def source(self):
-        self.run('git clone --depth=1 git@bitbucket.org:redlionstl/util-linux.git')
+        self.run('git clone --depth=1 git@bitbucket.org:redlionstl/util-linux.git -b v2.30.2')
 
     def build(self):
-        source_dir = os.path.join(self.build_folder, self.name)
-        build_dir = os.path.join(self.build_folder, 'build')
-        os.mkdir(build_dir)
+        ldflags = [
+            '-Wl,-rpath-link={0}'.format(os.path.join(self.folder, '.libs'))
+        ]
+        cflags = []
 
-        env = AutoToolsBuildEnvironment(self)
+        for requirement_name in self.requires:
+            requirement = self.deps_cpp_info[requirement_name]
+            for lib_dir in requirement.libdirs:
+                ldflags.append('-L' + os.path.join(requirement.rootpath, lib_dir))
+            for include_dir in requirement.includedirs:
+                cflags.append('-I' + os.path.join(requirement.rootpath, include_dir))
 
-        with tools.environment_append(RunEnvironment(self).vars):
-            self.run('sh {0}/autogen.sh'.format(source_dir))
+        extra_environment_vars = {
+            'LDFLAGS': ' '.join(ldflags),
+            'CFLAGS': ' '.join(cflags),
+            'CPPFLAGS': ' '.join(cflags)
+        }
 
-            with tools.chdir(build_dir):
-                env.configure(configure_dir=source_dir, args=['--disable-use-tty-group'])
-            env.make(args=['-C', build_dir, 'LDFLAGS=-Wl,-rpath-link=' + os.path.join(build_dir, '.libs')])
+        with tools.environment_append(extra_environment_vars):
+            self.run('sh {0}/autogen.sh'.format(self.folder))
+            env = AutoToolsBuildEnvironment(self)
+            with tools.chdir(self.folder):
+                env.configure(configure_dir=self.folder, args=['--disable-use-tty-group', '--prefix=/'])
+            env.make(args=['-C', self.folder])
 
     def package(self):
-        build_dir = os.path.join(self.build_folder, 'build')
-        self.run('make -C {0} DESTDIR={1} install'.format(build_dir, self.package_folder))
+        self.run('make -C {0} DESTDIR={1} install'.format(self.folder, self.package_folder))
 
     def package_info(self):
         self.cpp_info.includedirs = ['include', 'usr/include']
@@ -40,3 +53,7 @@ class UtilLinuxConan(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
         super(UtilLinuxConan, self).configure()
+
+    @property
+    def folder(self):
+        return os.path.join(self.build_folder, self.name)
