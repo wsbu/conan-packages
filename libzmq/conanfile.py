eacee56ca6db120331eaabc6cb4a8e668a23ad53
David Zemon <david.zemon@redlion.net>
2019-03-29 10:11:51 -0500

    Both ZeroMQ libs are named zmq

--- libzmq/conanfile.py
+++ libzmq/conanfile.py
@@ -55,7 +55,7 @@ class Libzmq(ConanFile):
         self.cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ['zmq', 'zmq-static']
+        self.cpp_info.libs = ['zmq']
 
     @property
     def cmake(self):
